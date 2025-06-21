fc7cd1745927087e5d5210ebffb3f8bc43ac5f1b
Aline Taylor <alineyui.taylor@gmail.com>
2025-06-21 15:22:36 -0500

    Done! That was great!

--- main.rb
+++ main.rb
@@ -87,4 +87,110 @@ unless height_in_ft<5
   puts "You can ride the rollercoaster!"
 end
 
-#
+#loop control practice problem: fizzbuzz
+i = 1
+while i<=20
+  if i%3==0 && i%5==0
+    puts "#{i}: FizzBuzz"
+  elsif i%3==0
+   puts "#{i}: Fizz"
+  elsif i%5==0
+   puts "#{i}: Buzz"
+  else
+    puts i
+  end
+  i+= 1
+end #FIRST TRY BABYYY: now THAT was satisfying
+
+#Built-in Ruby methods practice problems
+puts "123".to_i.class
+puts 123.to_s.class
+puts 123.45.to_f.class
+
+[1,2,3,4,5].each do |i|
+  puts i
+end
+
+{ "name" => "Alice", "age" => 30 }.each do |kv_pair|
+  puts kv_pair
+end
+
+#Final Pre-class practice exercises <3
+
+#1. Variables
+my_name2 = "Aline Yui Taylor"
+puts my_name2, my_name2.class
+
+#2. Arithmetic Operators
+width2 = 10
+height2 = 5
+rectangle_area2 = width2*height2
+puts rectangle_area2
+
+#3. Conditional Operators & Control Flow
+num = 0
+if num >0
+  puts "positive"
+elsif num<0
+  puts "negative"
+else
+  puts "zero"
+end
+
+#4. Basic Methods
+def add(a,b)
+  return a+b
+end
+puts add(1,2)
+
+#5. Testing with RSpec
+#maybe later lol
+
+#6. Arrays
+[1,2,3,4,5].each do |i|
+  puts i
+end
+
+#7. Hashes
+{ "name" => "Alice", "age" => 30 }.each do |k,v|
+  puts "#{k}:#{v}"
+end
+
+#8. Loops & Control Flow
+i = 1
+while i<=20
+  if i%3==0 && i%5==0
+    puts "#{i}: FizzBuzz"
+  elsif i%3==0
+    puts "#{i}: Fizz"
+  elsif i%5==0
+    puts "#{i}: Buzz"
+  else
+    puts i
+  end
+  i+= 1
+end
+
+#9. Super Fizz Buzz
+i = 1
+while i<=200
+  if i%3==0 && i%5==0 && i%7==0
+    puts "#{i}: FizzBuzzSuper"
+  elsif i%3==0 && i%5==0
+    puts "#{i}: FizzBuzz"
+  elsif i%3==0 && i%7==0
+    puts "#{i}: FizzSuper"
+  elsif i%5==0 && i%7==0
+    puts "#{i}: BuzzSuper"
+  elsif i%3==0
+    puts "#{i}: Fizz"
+  elsif i%5==0
+    puts "#{i}: Buzz"
+  elsif i%7==0
+    puts "#{i}: Super"
+  else
+    puts i
+  end
+  i+= 1
+end
+#done!
